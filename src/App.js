8e0eaf41e83131288580781e74688fed5c7d0464
GerardoS-C <gsandovalc1900@alumno.ipn.com>
2023-06-29 17:40:14 -0600

    ejemplos react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Example from './common/example';
+import Main_page from './pages/main_page';
+
 
 function App() {
   return (
     <div className="App">
       <Example></Example>
+      <Main_page></Main_page>
     </div>
   );
 }
---
+++ src/pages/main_page.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function Main_page(){
+    return(
+        <>
+            <div className="w-64 h-64 bg-black text-white">
+                <p >Hsadglj</p>
+            </div>
+        </>
+    )
+}
