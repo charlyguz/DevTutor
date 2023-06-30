f6fcc713ea2605b8e834082c3f41921bae870e0f
charlyguz <gusmanc622@gmail.com>
2023-06-29 20:28:40 -0600

    inicio de main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Example from './common/example';
-import Main_page from './pages/main_page';
-
+import Header from './common/header';
 
 function App() {
   return (
     <div className="App">
-      <Example></Example>
-      <Main_page></Main_page>
+      <Header/>
     </div>
   );
 }
---
+++ src/common/header.jsx
@@ -0,0 +1,61 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { MenuIcon } from '@heroicons/react/outline';
+
+const navigation = [
+  { name: 'Cursos', href: '#' },
+  { name: 'Mi Aprendizaje', href: '#' },
+  { name: 'Mi Perfil', href: '#' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Header() {
+  return (
+    <Popover className="relative bg-black">
+      <div className="max-w-5xl mx-auto px-10 sm:px-6">
+        <div className="flex justify-between items-center  py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button className="bg-black rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden md:flex space-x-10">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="text-base font-medium text-white hover:text-gray-300">
+                {item.name}
+              </a>
+            ))}
+          </Popover.Group>
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <a href="#" className="whitespace-nowrap text-base font-medium text-white hover:text-gray-300">
+              Sign in
+            </a>
+            <a
+              href="#"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-900 hover:bg-gray-400"
+            >
+              Sign up
+            </a>
+          </div>
+        </div>
+      </div>
+    </Popover>
+  )
+}
+
